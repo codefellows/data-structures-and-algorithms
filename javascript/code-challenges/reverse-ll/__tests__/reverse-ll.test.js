0993b32810aa3fc10016219e0ca7525f5b29fb46
John Cokos <highlander.44@gmail.com>
2020-09-22 13:01:52 -0700

    JS Sample Setup

---
+++ javascript/code-challenges/reverse-ll/__tests__/reverse-ll.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const reverse = require('../reverse-ll.js');
+
+describe('Reverse', () => {
+  it('works', () => {
+    expect(reverse()).toBeTruthy();
+  })
+})
---
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Require our linked list implementation
+const LinkedLilst = require('../index');
+
+describe('Linked List', () => {
+  it('works', () => {
+    expect(true).toBeTruthy();
+  });
+});
