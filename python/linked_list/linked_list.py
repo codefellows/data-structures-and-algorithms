d283411fafe27a08f48def944223a3f9882cd67f
MasonChance <mason.fryberger@gmail.com>
2020-12-19 19:04:42 -0800

    initial feature tests passing for Linked list instantiation

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,80 @@
 class LinkedList:
     """
-    Put docstring here
+    builds an instance of a Singly Linked List. all relevant methods have their own docstrings
+
+    Input <-- head, value: default='None'
+    Output --> Object: instance='linked-list'
+
+    Time: O(1)
+    Space: O(1)
     """
 
-    def __init__(self):
+    def __init__(self, value=None):
         # initialization here
-        pass
+        self.head = Node(value)
+        
 
-    def some_method(self):
+    def insert(self, value):
         # method body here
-        pass
+        node = Node(value)
+
+        if self.head is not None:
+            node.next = self.head
+        self.head = node
+    
+    def includes(self, check):
+        """ searches linked list for a value returns true if yes false if no. 
+        Code based on psuedo code from reading asssingment @:https://codefellows.github.io/common_curriculum/data_structures_and_algorithms/Code_401/class-05/resources/singly_linked_list.html
+        """
+        current = self.head
+        
+        while current is not None:
+
+            if current.value == check:
+                return True
+            else:
+               current = current.next
+                
+        return False
+        
+    def __str__(self):
+        once_and_future_string = []
+
+        current = self.head
+
+        while current is not None:
+             
+                once_and_future_string.append(f'{{ {current.value} }} ->')
+                current = current.next
+                
+                
+        once_and_future_string.append('None ')        
+        console_log = ' '.join(once_and_future_string)
+        return console_log
+               
+            
+                
+
+class Node:
+    """ Constructs Nodes with values and a reference to next for use with LinkedList
+    
+    Input <-- value: any, reference to next node
+    Output --> object: node w/value and reference
+    
+    """ 
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+
+
+
+if __name__ == "__main__":
+    finding_francis = LinkedList('goons')
+    finding_francis.insert('meat-head')
+    finding_francis.insert('brown-pants')
+    finding_francis.insert('car-goon')
+
+    finding_francis.includes('goons')
+    
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,108 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
 
+import pytest
 
 def test_import():
     assert LinkedList
+
+def test_instance():
+    finding_francis = LinkedList('goons')
+    assert finding_francis.head
+    
+def test_node_import():
+    assert Node
+
+def test_node_instance_value():
+    # !!! previously written in the syntax of 
+    # `assert lackey.value` received linter error in spite of test passing.???
+    lackey = Node('meat-head')
+    actual = lackey.value
+    expected = 'meat-head'
+    assert actual == expected
+
+def test_node_instance_reference():
+    lackey = Node('meat-head')
+    actual = lackey.next
+    expected = None
+    assert actual == expected
+
+def test_llist_instance_insert():
+    """testing when head was originally empty llist instance
+    """
+    finding_francis = LinkedList('goons')
+    finding_francis.insert('meat-head')
+    
+    actual = finding_francis.head.value
+    expected = 'meat-head'
+    assert actual == expected
+    
+def test_llist_next_post_insert():
+    finding_francis = LinkedList('goons')
+    finding_francis.insert('meat-head')
+    actual = finding_francis.head.next.value
+    expected = 'goons'
+    assert actual == expected
+
+def test_next_of_origin_node():
+    """tests next of the original head Node upon list instantiation
+    """
+    finding_francis = LinkedList('goons')
+    finding_francis.insert('meat-head')
+
+def test_includes_method_true():
+    finding_francis = LinkedList('goons')
+    finding_francis.insert('meat-head')
+    finding_francis.insert('brown-pants')
+    finding_francis.insert('car-goon')
+    
+    assert finding_francis.includes('meat-head') == True
+
+# @pytest.mark.skip('checking base')
+def test_includes_method_false():
+    finding_francis = LinkedList('goons')
+    finding_francis.insert('meat-head')
+    finding_francis.insert('brown-pants')
+    finding_francis.insert('car-goon')
+
+    actual = finding_francis.includes('ryan-reynalds') 
+    expected = False
+    assert  actual == expected
+
+def test_str_of_llist_class():
+    finding_francis = LinkedList('goons')
+    finding_francis.insert('meat-head')
+    finding_francis.insert('brown-pants')
+    finding_francis.insert('car-goon')
+
+    actual = finding_francis.__str__()
+    expected = '{ car-goon } -> { brown-pants } -> { meat-head } -> { goons } -> None '
+    assert actual == expected
+
+
+
+
+
+
+
+############
+# Fixtures #
+############
+
+# initial attempt at using fixture revealed I need to understand their construction and behaavior better before implementing in code. TODO: when time permits, clean up testing by adding fixtures. 
+
+# @pytest.fixture
+# def finding_francis_data():
+#     finding_francis = LinkedList('goons')
+#     return finding_francis
+
+
+# @pytest.fixture
+# def lackey_data():
+#     lackey = Node('lackey')
+#     return lackey
+
+
+# @pytest.fixture(autouse=True)
+# def francis_not_here():
+    # may not be need. I should see what returns when i use a fixture before constructing the cleanup function. 
+
