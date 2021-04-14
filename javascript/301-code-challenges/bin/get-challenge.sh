6dc8083c7e2f3bafa57f92a2235fcf58c358c28a
John Cokos <highlander.44@gmail.com>
2021-04-14 11:37:22 -0700

    Get Code Challenge now pulls from the right folders

--- javascript/301-code-challenges/bin/get-challenge.sh
+++ javascript/301-code-challenges/bin/get-challenge.sh
@@ -2,4 +2,4 @@
 
 echo "Getting Code Challenge $1"
 
-`which curl` -s https://codefellows.github.io/code-301-guide/curriculum/class-$1/challenges/challenges-$1.test.js > ./code-challenges/challenges-$1.test.js
+`which curl` -s https://codefellows.github.io/code-301-guide/curriculum/class-$1/challenges/challenges-$1.test.js > ./301-code-challenges/challenges-$1.test.js
