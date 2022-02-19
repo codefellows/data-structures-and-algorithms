34051fd2fe9c6d3252119f8714870d39a5713e26
JB-Tellez <jb.tellez@gmail.com>
2022-02-19 10:08:23 -0800

    adds linked_list_zip starter test

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,1 +1,2 @@
-from data_structures.linked_list import LinkedList
+def zip_lists(a, b):
+    pass
---
+++ python/tests/code_challenges/test_linked_list_zip.py
@@ -0,0 +1,5 @@
+from code_challenges.linked_list_zip import zip_lists
+
+
+def test_exists():
+    assert zip_lists
