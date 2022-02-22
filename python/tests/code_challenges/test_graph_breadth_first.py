5b798909a7153c5a9c86d99ab3216532aad436cf
JB-Tellez <jb.tellez@gmail.com>
2022-02-21 21:52:05 -0800

    adds graph bfs test

--- python/tests/code_challenges/test_graph_breadth_first.py
+++ python/tests/code_challenges/test_graph_breadth_first.py
@@ -7,7 +7,10 @@ def test_exists():
 
 @pytest.mark.skip("TODO")
 def test_bfs(graph):
-    pass
+    actual = graph.breadth_first(graph.get_nodes[0])
+    expected = ["a","b","c","d","e","f","g","h","i","j"]
+    assert actual == expected
+
 
 
 
@@ -24,16 +27,43 @@ def graph():
     f = letters.add_node("f")
     g = letters.add_node("g")
     h = letters.add_node("h")
+    i = letters.add_node("i")
+    j = letters.add_node("j")
 
     letters.add_edge(a, b)
-    letters.add_edge(b, c)
-    letters.add_edge(c, g)
-    letters.add_edge(a, d)
+    letters.add_edge(a, c)
+    letters.add_edge(a, e)
 
+    letters.add_edge(b, a)
+    letters.add_edge(b, d)
+
+    letters.add_edge(c, a)
+    letters.add_edge(c, f)
+
+    letters.add_edge(d, b)
     letters.add_edge(d, e)
-    letters.add_edge(d, h)
-    letters.add_edge(d, f)
+
+
+    letters.add_edge(e, a)
+    letters.add_edge(e, d)
+    letters.add_edge(e, f)
+    letters.add_edge(e, g)
+
+    letters.add_edge(f, c)
+    letters.add_edge(f, g)
+    letters.add_edge(f, h)
+
+    letters.add_edge(g, e)
+    letters.add_edge(g, h)
 
     letters.add_edge(h, f)
+    letters.add_edge(h, g)
+    letters.add_edge(h, j)
+
+    letters.add_edge(i, f)
+    letters.add_edge(i, j)
+
+    letters.add_edge(j, h)
+    letters.add_edge(j, i)
 
     return letters
