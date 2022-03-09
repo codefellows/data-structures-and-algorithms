eed4d5269debb8144eea800fbd0aac952b31a996
JB-Tellez <jb.tellez@gmail.com>
2022-03-09 09:08:02 -0800

    restores starter code for Queue

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,31 +1,12 @@
-from collections import deque
-
-
 class Queue:
-    """Queue implementation using deque under the hood"""
+    """
+    Put docstring here
+    """
 
     def __init__(self):
-        self.storage = deque()
+        # initialization here
+        pass
 
-    def enqueue(self, value):
-        self.storage.append(value)
-
-    def dequeue(self):
-        if self.is_empty():
-            raise EmptyError(self.dequeue)
-
-        return self.storage.popleft()
-
-    def peek(self):
-        if self.is_empty():
-            raise EmptyError(self.peek)
-
-        return self.storage[0]
-
-    def is_empty(self):
-        return len(self.storage) == 0
-
-
-class EmptyError(Exception):
-    def __init__(self, method):
-        super().__init__(f"Cannot call {method.__name__} method on empty Queue")
+    def some_method(self):
+        # method body here
+        pass
