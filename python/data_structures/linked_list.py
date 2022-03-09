1cd817faaf052a49ae6d1e076aeb482c6ff631c2
JB-Tellez <jb.tellez@gmail.com>
2022-03-09 00:29:04 -0800

    adds reference kary tree

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -4,13 +4,100 @@ class LinkedList:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+
+        current = self.head
+        values = []
+
+        while current:
+            values.append("{ " + current.value + " }")
+            current = current.next
+
+        values.append("NULL")
+
+        return " -> ".join(values)
+
+    def insert(self, value):
+        self.head = Node(value, self.head)
+
+    def includes(self, value):
+        current = self.head
+
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+
+        return False
+
+    def append(self, value):
+        current = self.head
+        node = Node(value)
+
+        if not current:
+            self.head = node
+            return
+
+        while current.next:
+            current = current.next
+
+        current.next = node
+
+    def insert_before(self, before_value, value):
+        current = self.head
+        if not current:
+            raise TargetError
+
+        if current.value == before_value:
+            self.head = Node(value, self.head)
+            return
+
+        while current.next:
+            if current.next.value == before_value:
+                current.next = Node(value, current.next)
+                return
+            current = current.next
+
+        raise TargetError
+
+    def insert_after(self, after_value, value):
+        current = self.head
+        while current:
+            if current.value == after_value:
+                current.next = Node(value, current.next)
+                return
+            current = current.next
+
+        raise TargetError
+
+    def kth_from_end(self, k):
+        if k < 0:
+            raise TargetError
+
+        current = self.head
+        length = 0
+        while current:
+            current = current.next
+            length += 1
+        steps_remaining = length - k - 1
+        if steps_remaining < 0:
+            raise TargetError
+
+        current = self.head
+        while steps_remaining:
+            current = current.next
+            steps_remaining -= 1
+
+        return current.value
 
 
-class TargetError:
+class TargetError(Exception):
     pass
+
+
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next = next_
--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,31 @@
+from collections import deque
+
+
 class Queue:
-    """
-    Put docstring here
-    """
+    """Queue implementation using deque under the hood"""
 
     def __init__(self):
-        # initialization here
-        pass
+        self.storage = deque()
 
-    def some_method(self):
-        # method body here
-        pass
+    def enqueue(self, value):
+        self.storage.append(value)
+
+    def dequeue(self):
+        if self.is_empty():
+            raise EmptyError(self.dequeue)
+
+        return self.storage.popleft()
+
+    def peek(self):
+        if self.is_empty():
+            raise EmptyError(self.peek)
+
+        return self.storage[0]
+
+    def is_empty(self):
+        return len(self.storage) == 0
+
+
+class EmptyError(Exception):
+    def __init__(self, method):
+        super().__init__(f"Cannot call {method.__name__} method on empty Queue")
