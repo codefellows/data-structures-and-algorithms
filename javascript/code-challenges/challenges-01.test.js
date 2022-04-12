77d407803d8305513ebe2f1e96a5627344a2860c
Anthony Hill <anthonymhill23@gmail.com>
2022-04-12 16:53:53 -0400

    finished challenge one

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -154,6 +154,12 @@ Return the resulting output array.
 
 const fizzbuzz = (arr) => {
   // Solution code here...
+  for (var i=1; i < 101; i++){
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
 };
 
 /* ------------------------------------------------------------------------------------------------
