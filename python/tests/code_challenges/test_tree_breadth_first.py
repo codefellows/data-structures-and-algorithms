ca3bbf676798fc4433ea6f6e388be844053437e9
JB-Tellez <jb.tellez@gmail.com>
2022-05-12 15:52:17 -0700

    fixes tree-breadth-first-search tests to match required implementation

--- python/tests/code_challenges/test_tree_breadth_first.py
+++ python/tests/code_challenges/test_tree_breadth_first.py
@@ -3,15 +3,9 @@ from data_structures.binary_tree import BinaryTree, Node
 from code_challenges.tree_breadth_first import breadth_first
 
 
+@pytest.mark.skip("TODO")
 def test_exists():
     assert breadth_first
-
-
-@pytest.mark.skip("TODO")
-def test_none_tree():
-    expected = []
-    actual = breadth_first(None)
-    assert actual == expected
 
 
 @pytest.mark.skip("TODO")
@@ -25,40 +19,31 @@ def test_rootless_tree():
 @pytest.mark.skip("TODO")
 def test_single_node():
     tree = BinaryTree()
-    tree.add("apples")
+    tree.root = Node("apples")
     expected = ["apples"]
     actual = breadth_first(tree)
     assert actual == expected
 
 
 @pytest.mark.skip("TODO")
-def test_add_two_nodes():
-    tree = BinaryTree()
-    tree.add("apples")
-    tree.add("bananas")
-    assert tree._root.value == "apples"
-    assert tree._root.left.value == "bananas"
-
-
-@pytest.mark.skip("TODO")
 def test_two_nodes():
     tree = BinaryTree()
-    tree.add("apples")
-    tree.add("bananas")
+    tree.root = Node("apples")
+    tree.root.right = Node("bananas")
     expected = ["apples", "bananas"]
-    actual = BinaryTree.breadth_first(tree)
+    actual = breadth_first(tree)
     assert actual == expected
 
 
 @pytest.mark.skip("TODO")
 def test_four_nodes():
     tree = BinaryTree()
-    tree.add("apples")
-    tree.add("bananas")
-    tree.add("cucumbers")
-    tree.add("dates")
+    tree.root = Node("apples")
+    tree.root.left = Node("bananas")
+    tree.root.right = Node("cucumbers")
+    tree.root.right.right = Node("dates")
     expected = ["apples", "bananas", "cucumbers", "dates"]
-    actual = BinaryTree.breadth_first(tree)
+    actual = breadth_first(tree)
     assert actual == expected
 
 
@@ -67,18 +52,41 @@ def test_example_from_reading():
     """
     We build these out by hand because the example has some gaps
     i.e. it is not added to left-to-right
+
+                            2
+                7                   5
+        2               6                       9
+                    5       11              4
+
+    result = [2,7,5,2,6,9,5,11,4]
     """
     tree = BinaryTree()
 
-    level_3_2 = Node(2)
-    level_3_6 = Node(6, Node(5), Node(11))
-    level_3_9 = Node(9, Node(4))
+    level_0 = Node(2)
+    level_1_first = Node(7)
+    level_1_second = Node(5)
 
-    level_2_left = Node(7, level_3_2, level_3_6)
-    level_2_right = Node(5, right=level_3_9)
+    level_2_first = Node(2)
+    level_2_second = Node(6)
+    level_2_third = Node(9)
 
-    tree._root = Node(2, level_2_left, level_2_right)
+    level_3_first = Node(5)
+    level_3_second = Node(11)
+    level_3_third = Node(4)
+
+    tree.root = level_0
+    level_0.left = level_1_first
+    level_0.right = level_1_second
+    level_1_first.left = level_2_first
+    level_1_first.right = level_2_second
+    level_1_second.right = level_2_third
+
+    level_2_second.left = level_3_first
+    level_2_second.right = level_3_second
+
+    level_2_third.right = level_3_third
 
     expected = [2, 7, 5, 2, 6, 9, 5, 11, 4]
-    actual = BinaryTree.breadth_first(tree)
+    actual = breadth_first(tree)
+
     assert actual == expected
