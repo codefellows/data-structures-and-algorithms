6341e4957b7a972070013a19d38ec88093b2fdd8
Jae Loney <jaimieloney@gmail.com>
2022-06-10 00:48:10 -0400

    Code Challenge 7

---
+++ python/code_challenges/linked_list_kth/README.md
@@ -0,0 +1,28 @@
+# Singly Linked List
+A challenge of finding a given positions value in a linked list with a stretch goal of finding the midpoint value in a list.
+
+## Challenge
+Create kth_from_end and midpoint methods as well as tests pass the tests in the data_structures folder for said methods.
+
+## Approach & Efficiency
+The Approach was to use while loops to traverse the linked lists. This is O(N)
+
+## API
+Kth_from_end method to find a value a certain distance from the end of the linked list.
+Midpoint method to find the middle value in the list.
+
+# Resources
+Worked with Aoife, JJ, and Tony on the whiteboard and on writing the code for this challenge.
+The raw whiteboard can be found here: https://jaimierl494930.invisionapp.com/freehand/Untitled-kaVgArNpl
+https://www.geeksforgeeks.org/linked-list-set-2-inserting-a-node/
+
+https://towardsdatascience.com/how-to-define-custom-exception-classes-in-python-bfa346629bca for TargetError creation
+
+# Whiteboard
+For the Summary, Description, Approach & Efficiency, Solution, please see the whiteboard below:
+![See my work here](/python/code_challenges/linked_list_kth/CCWB7.drawio.png)
+
+# Code
+[See the Code](python/code_challenges/linked_list_kth/linked_list.py), [See the test file](python/code_challenges/linked_list_insertions/test_linked_list_lth.py)
+
+To go back to the main ReadMe [Click Here](../../README.md)
---
+++ python/code_challenges/linked_list_kth/test_linked_list_kth.py
@@ -0,0 +1,63 @@
+import pytest
+from .linked_list_kth import LinkedList, TargetError
+
+#@pytest.mark.skip("TODO")
+def test_kth_from_end_zero():
+    linked_list = LinkedList()
+    values = ["apples", "bananas", "cucumbers"]
+    for value in reversed(values):
+        linked_list.insert(value)
+    actual = linked_list.kth_from_end(0)
+    expected = "cucumbers"
+    assert actual == expected
+
+
+#@pytest.mark.skip("TODO")
+def test_kth_from_end_one():
+    linked_list = LinkedList()
+    values = ["apples", "bananas", "cucumbers"]
+    for value in reversed(values):
+        linked_list.insert(value)
+    actual = linked_list.kth_from_end(1)
+    expected = "bananas"
+    assert actual == expected
+
+
+#@pytest.mark.skip("TODO")
+def test_kth_from_end_two():
+    linked_list = LinkedList()
+    values = ["apples", "bananas", "cucumbers"]
+    for value in reversed(values):
+        linked_list.insert(value)
+    actual = linked_list.kth_from_end(2)
+    expected = "apples"
+    assert actual == expected
+
+
+#@pytest.mark.skip("TODO")
+def test_kth_from_end_out_of_range():
+    linked_list = LinkedList()
+    values = ["apples", "bananas", "cucumbers"]
+    for value in reversed(values):
+        linked_list.insert(value)
+    with pytest.raises(TargetError):
+        linked_list.kth_from_end(3)
+
+
+#@pytest.mark.skip("TODO")
+def test_kth_from_end_under_range():
+    linked_list = LinkedList()
+    values = ["apples", "bananas", "cucumbers"]
+    for value in reversed(values):
+        linked_list.insert(value)
+    with pytest.raises(TargetError):
+        linked_list.kth_from_end(-1)
+
+
+#@pytest.mark.skip("TODO")
+def test_kth_from_end_size_one():
+    linked_list = LinkedList()
+    linked_list.insert("apples")
+    actual = linked_list.kth_from_end(0)
+    expected = "apples"
+    assert actual == expected
