82e5088710e7d0bd29f9cbfd1c5cca32d64aa230
Jae Loney <jaimieloney@gmail.com>
2022-07-15 00:22:42 -0400

    Code in the right place still not passing

--- python/code_challenges/breadth_first/tree_breadth_first.py
+++ python/code_challenges/breadth_first/tree_breadth_first.py
@@ -1,5 +1,26 @@
 from .binary_tree import BinaryTree
+from .queue import Queue
+from .invalid_operation_error import InvalidOperationError
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+
+    tree_queue = Queue()
+    tree_list = []
+
+    try:
+        if not tree.root:
+            pass
+        else:
+            tree_queue.enqueue(tree.root)
+            while tree_queue.front:
+                if front_node.left:
+                    tree_queue.enqueue(front_node.left)
+                if front_node.right:
+                    tree_queue.enqueue(front_node.right)
+                front_node = tree_queue.dequeue(tree_queue.front)
+                tree_list.append(front_node.value)
+        return tree_list
+
+    except:
+        raise InvalidOperationError
