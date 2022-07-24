4df0e877c3807ac135202e8e93b8e4fd66f4a72c
Jae Loney <jaimieloney@gmail.com>
2022-07-24 14:13:37 -0400

    None case is not working. All other tests pass

--- python/code_challenges/breadth_first/tree_breadth_first.py
+++ python/code_challenges/breadth_first/tree_breadth_first.py
@@ -1,26 +1,32 @@
-from .binary_tree import BinaryTree
 from .queue import Queue
+from .binary_tree import BinaryTree, Node
 from .invalid_operation_error import InvalidOperationError
 
 
 def breadth_first(tree):
-
     tree_queue = Queue()
+
     tree_list = []
 
-    try:
-        if not tree.root:
-            pass
-        else:
-            tree_queue.enqueue(tree.root)
-            while tree_queue.front:
-                if front_node.left:
-                    tree_queue.enqueue(front_node.left)
-                if front_node.right:
-                    tree_queue.enqueue(front_node.right)
-                front_node = tree_queue.dequeue(tree_queue.front)
-                tree_list.append(front_node.value)
-        return tree_list
+    # if tree.root == None:
+    #     print("inside")
+    #     return
 
-    except:
-        raise InvalidOperationError
+    tree_queue.enqueue(tree.root)
+
+    while tree_queue.front:
+        front_node = tree_queue.dequeue()
+        tree_list.append(front_node.value)
+
+        if front_node.left:
+            tree_queue.enqueue(front_node.left)
+
+        if front_node.right:
+            tree_queue.enqueue(front_node.right)
+
+    print(tree_list)
+    return tree_list
+
+if __name__ == "__main__":
+    tree = BinaryTree()
+    breadth_first(tree)
